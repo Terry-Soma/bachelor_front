85ce0ed0b04dd7fb0ce3429f2d8c31b1aef94a8f
Terry-Soma <jackhack116@yahoo.com>
2023-06-02 04:03:54 +0800

    before publish

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:1234/api/v1',
+  baseURL: 'https://elselt-ikhzasag.onrender.com/api/v1',
 });
 // const instance = axios.create({
 //   baseURL: 'http://localhost:1234/api/v1',
