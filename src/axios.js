706014873b9eb25abfb6077dd90e498d85e7ffb1
Terry-Soma <jackhack116@yahoo.com>
2023-06-02 14:05:14 +0800

    before bagsh

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-// const instance = axios.create({
-//   baseURL: 'https://elselt-ikhzasag.onrender.com/api/v1',
-// });
 const instance = axios.create({
-  baseURL: 'http://localhost:1234/api/v1',
+  baseURL: 'https://elselt-ikhzasag.onrender.com/api/v1',
 });
+// const instance = axios.create({
+//   baseURL: 'http://localhost:1234/api/v1',
+// });
 instance.defaults.withCredentials = true;
 export default instance;
